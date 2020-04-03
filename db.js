38446168e808070a2600243b606526101c50eb1e
unknown <hamish.tana24@gmail.com>
2020-04-03 13:21:13 +1300

    added update function and fixed addnewreport with Raymond

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ module.exports = {
   getAllOffenderNames,
   getAllReports,
   addNewOffender,
-  addNewReport
+  addNewReport,
+  updateReport
 }
 
 function getAllOffenderNames(db = connection) {
@@ -13,7 +14,9 @@ function getAllOffenderNames(db = connection) {
 }
 
 function getAllReports(db = connection) {
-  return db('reports').select()
+  return db('offenders')
+  .join('reports', 'offenders.id', 'reports.offender_id')
+  .select('reports.id as reportsId', 'reports.image as reportsImage', 'reports.comment', 'reports.offender_id as reports.offenderId', 'reports.location', 'reports.violation', 'reports.date', 'offenders.id', 'offenders.image', 'offenders.name')
 }
 
 function addNewReport(reportData, db = connection) {
@@ -23,10 +26,39 @@ function addNewReport(reportData, db = connection) {
 
 
 function addNewOffender(reportData, db = connection) {
-  const { offenderName, offenderImage } = reportData;
+  const { offenderName } = reportData;
   return db('offenders').insert({ name: offenderName, image: offenderImage })
 }
 
-// function updateReport(db = connection) {
+function updateReport(reportData, db = connection) {
+  const { image, reportOffenderId, comment, location, date, violation } = reportData;
+return db('reports')
+  .where({ 
+    location: location,
+    comment: comment,
+    violation: violation,
+    date: date
+  })
+    .update({ 
+  comment: comment, 
+  location: location, 
+  image: image, 
+  date: date, 
+  violation: violation, 
+  offender_id: reportOffenderId })
+}
 
+function getTopOffenders() {
+  
+}
+
+// FYR hamish
+// function updateTask(id, taskName, db = database) {
+//   return db('todo')
+//     .where('id' id)
+//     .update({ task: taskName })
 // }
+
+// knex('users')
+//   .where({ id: 2 })
+//   .update({ name: 'Homer' })
