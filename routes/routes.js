04e650169434661e6e5b3f5eb2a78fe9d20417fa
Pat-Lim <impatlim@gmail.com>
2020-04-03 13:19:35 +1300

    Fixed submit reports route

--- routes/routes.js
+++ routes/routes.js
@@ -5,26 +5,30 @@ const db = require('../db')
 router.get('/', (req, res) => {
   db.getTopOffenders()
     .then(offenders => {
-      const test_offenders = [{
-        name: 'don',
-      }, {
-        name: 'emily'
-      }, {
-        name: 'lane'
+      const testOffenders = [{
+        name: "don",
+        id: 2,
+        image: "broken"
       }]
-      res.render('index', test_offenders)
+      res.render('index', testOffenders)
     })
     .catch(getError(res))
 })
 
 router.get('/reports', (req, res) => {
-  res.render('report')
+  db.getAllReports()
+  .then(reports => {
+    res.render('report', reports)
+  })
 })
 
 router.post('/reports', (req, res) => {
   const { image, offenderName, comment, location, date, violation } = req.body
-  db.addNewReport({ image, offenderName, comment, location, date, violation })
+  db.addNewOffender(offenderName).
+  then(id => {
+    db.addNewReport({ image, reportOffenderId = id, comment, location, date, violation })
     .then(res.redirect('/'))
+  })
 })
 
 router.get('/rules', (req, res) => {
@@ -32,9 +36,12 @@ router.get('/rules', (req, res) => {
     .then(res.render('rules'))
 })
 
-router.get('/offenders', (req, res) => {
-  db.getAllOffenderNames()
-    .then(res.render('offenders'))
+router.get('/violations/:id', (req, res) => {
+  const id = req.params.id
+  db.getViolationById(id)
+  .then(result => {
+    res.render('violations', result))
+  })
 })
 
 module.exports = router
